b8ba047acaacabf20e39906dac56d3ca89754533
Iurii Bairamukov <juribai@mail.ru>
2020-12-19 15:25:30 +0700

    feat: добавить helmet, обработчики ошибок

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const NotFoundError = require('./errors/not-found-error');
 const router = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
+const notFound = require('./middlewares/notFound');
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/newsdb', {
@@ -16,32 +19,16 @@ mongoose.connect('mongodb://localhost:27017/newsdb', {
 });
 
 app.use(cors());
+app.use(helmet());
 app.use(bodyParser.json());
 
 const { PORT = 3000 } = process.env;
 
 app.use(requestLogger);
-
 app.use(router);
-
 app.use(errorLogger);
-
 app.use(errors());
-
-app.use((req, res, next) => {
-  next(new NotFoundError('Запрашиваемый ресурс не найден'));
-});
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT);
